00f766c97b97397678d849c95b534c6eadbe39ee
LiShuxue <1149926505@qq.com>
2018-08-16 18:00:19 +0800

    home页面添加轮播图

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,54 @@
 <template>
   <div class="home">
-    <h1>This is home page</h1>
+    <div class="banner">
+      <swiper :options="swiperOption">
+        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
+        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
+        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
+        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
+        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
+        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
+        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
+        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
+        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
+        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
   </div>
 </template>
+
+<script>
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+export default {
+  data() {
+    return {
+      swiperOption: {
+        spaceBetween: 20, // 切换图片时中间的白条
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        }
+      }
+    }
+  },
+
+  components: {
+    swiper,
+    swiperSlide
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.banner{
+  width: 60vw;
+  height: 25vh;
+}
+</style>
