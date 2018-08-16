fd036e7e99823afb9f36b7bc8c0fc137b421a3de
LiShuxue <1149926505@qq.com>
2018-08-16 13:17:02 +0800

    初始化页面整体布局

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,69 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="app">
+    <div class="header">header</div>
+
+    <div class="content">
+      <div class="left">
+        left
+        <nav-bar></nav-bar>
+      </div>
+      <div class="center">
+        center
+        <router-view/>
+      </div>
+      <div class="right">right</div>
     </div>
-    <router-view/>
+
+    <div class="footer">footer</div>
+    
   </div>
 </template>
 
-<style>
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style scoped>
+.app{
+  background-color: #EEEEEE;
+}
+
+.header{
+  background-color: #F8F8F8;
+  height: 10vh;
+}
+
+.content{
+  display: flex;
+  flex-direction: row;
+  height: 80vh;
+}
+
+.footer{
+  background-color: #F8F8F8;
+  height: 10vh;
+}
+
+.left{
+  order: 0;
+  width: 20vw;
+}
+
+.center{
+  order: 1;
+  width: 60vw;
+  background-color: #b6b6b6;
+}
+
+.right{
+  order: 2;
+  width: 20vw;
+}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,6 @@
+<template>
+  <nav class="nav">
+    <li><router-link to="/">Home</router-link></li>
+    <li><router-link to="/about">About</router-link></li>
+  </nav>
+</template>
