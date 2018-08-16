5ac56f821d6727d55c1d54c46676d280301d7416
LiShuxue <1149926505@qq.com>
2018-08-16 15:07:09 +0800

    完成header部分

---
+++ src/components/header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <router-link to="/"><sapn class="logo"></sapn></router-link>
+    <router-link to="/"><span class="title">身体和灵魂，总有一个要在路上</span></router-link>
+  </div>
+</template>
+
+<style scoped>
+.header{
+  position: relative;
+  box-sizing: border-box;
+  height: 10vh;
+  background-color: #F8F8F8;
+}
+.logo{
+  position: absolute;
+  top: 4vh;
+  left: 3vw;
+  width: 80px;
+  height: 20px;
+  background: url('../assets/img/logo.png') no-repeat;
+  background-size: contain;
+}
+.title{
+  position: absolute;
+  top: 4vh;
+  left: 20vw;
+  font-family: 'Courier New', Courier, monospace;
+  color: #2192F5;
+}
+</style>
