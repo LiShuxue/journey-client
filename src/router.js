ef442ce2ec379ca1ab465fdc084751add0aaab52
LiShuxue <1149926505@qq.com>
2018-08-16 16:57:52 +0800

    通过keepAlive缓存home页

--- src/router.js
+++ src/router.js
@@ -11,7 +11,8 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: { keepAlive: true } 
     },
     {
       path: '/about',
