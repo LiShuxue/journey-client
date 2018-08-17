815286a7bfb03dcbb642b846942324a4f3665cad
LiShuxue <1149926505@qq.com>
2018-08-17 09:16:13 +0800

    将swiper单独抽到一个文件中

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,54 +1,17 @@
 <template>
   <div class="home">
-    <div class="banner">
-      <swiper :options="swiperOption">
-        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
-        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
-        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
-        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
-        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
-        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
-        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
-        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
-        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
-        <swiper-slide><img src="../assets/img/slide.png"></swiper-slide>
-        <div class="swiper-pagination" slot="pagination"></div>
-      </swiper>
-    </div>
+    <swipe-banner></swipe-banner>
   </div>
 </template>
 
 <script>
-import 'swiper/dist/css/swiper.css'
-import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import SwipeBanner from '@/components/SwipeBanner.vue'
 export default {
-  data () {
-    return {
-      swiperOption: {
-        spaceBetween: 20, // 切换图片时中间的白条
-        centeredSlides: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false
-        },
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true
-        }
-      }
-    }
-  },
-
   components: {
-    swiper,
-    swiperSlide
+    SwipeBanner
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.banner{
-  width: 60vw;
-  height: 25vh;
-}
 </style>
