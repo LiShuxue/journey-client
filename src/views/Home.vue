d64e5c0e2187c866d0c2788d7cf4a3b1369742f0
Li Shuxue <Shuxue.Li@sc.com>
2018-08-17 13:30:56 +0800

    动态绑定博客的相关数据

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <swipe-banner></swipe-banner>
-    <blog-item></blog-item>
+    <div class="blog-list" v-for="(item, index) in blogList" :key="index">
+      <blog-item :blog="item"></blog-item>
+    </div>
   </div>
 </template>
 
@@ -9,6 +11,28 @@
 import SwipeBanner from '@/components/SwipeBanner.vue'
 import BlogItem from '@/components/BlogItem.vue'
 export default {
+  data () {
+    return {
+      blogList: [
+        {
+          title: '我是标题',
+          subTitle: '我是子标题',
+          image: require('../assets/img/slide.png'),  // 这里需要用require，因为webpack已经将图片打包处理，渲染出来的路径不对，所以需要用require
+          time: '2018-08-17',
+          eye: '500',
+          like: '30',
+          category: 'NodeJS',
+          comment: {
+            count: '20',
+            list: [
+              {time: '2018-08-17', content: '我是评论', arthur: 'test user'}
+            ]
+          }
+        }
+      ]
+    }
+  },
+
   components: {
     SwipeBanner,
     BlogItem
