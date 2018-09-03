e4204e84494fbf66f72b859ee0df9a96217ffe5a
Li Shuxue <Shuxue.Li@sc.com>
2018-09-03 16:57:08 +0800

    修改router结构，添加管理员入口

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -10,17 +9,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
-      meta: { keepAlive: true }
+      component: () => import('./views/Main.vue'),
+      redirect: '/home',
+      children: [
+        { path: 'home',
+          component: () => import('./views/Home.vue'),
+          meta: { keepAlive: true }
+        },
+        { path: 'about', component: () => import('./views/About.vue') }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/admin',
+      component: () => import('./sever/Admin.vue'),
+      redirect: '/admin/login',
+      children: [
+        { path: 'login', component: () => import('./sever/login.vue') }
+      ]
     }
   ]
 })
