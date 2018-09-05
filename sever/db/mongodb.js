fa742a2ef105ebacf2cc4b3aa819231efdb478d4
Li Shuxue <Shuxue.Li@sc.com>
2018-09-05 15:05:56 +0800

    创建user模型

--- sever/db/mongodb.js
+++ sever/db/mongodb.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const db_config = require('./db_config');
 
 mongoose.connect('mongodb://' + db_config.host + ':' + db_config.port + '/' + db_config.name);
+mongoose.Promise =  global.Promise
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
+db.once('open', ()=>{
   console.log('DB connected...')
 });
 
