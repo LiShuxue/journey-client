b78e08e3e5515ef3f235919e41fd2faee98af664
LiShuxue <1149926505@qq.com>
2018-09-05 15:05:56 +0800

    创建user模型

---
+++ sever/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: { unique: true, type: String },
+    password: { type: String}
+},{
+    collection: 'User' // 加上这个就不会在程序中定义的是User而真实数据库中变成了Users
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
