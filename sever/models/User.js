5ea24f52d46a6bee4705c246ccf1cd0b75656073
Li Shuxue <Shuxue.Li@sc.com>
2018-09-06 14:10:19 +0800

    登录或者注册成功的时候生成token并存进数据库

--- sever/models/User.js
+++ sever/models/User.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     username: { unique: true, type: String },
-    password: { type: String}
+    password: { type: String },
+    token: { type: String }
 },{
     collection: 'User' // 加上这个就不会在程序中定义的是User而真实数据库中变成了Users
 });
