b32950f329943edce5a0fed725dc320895e43802
Li Shuxue <Shuxue.Li@sc.com>
2018-09-06 09:16:02 +0800

    fix jshint error

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -16,13 +16,13 @@ export default{
   data () {
     return {
       navList: [
-        {path: '/', name: '主页', style: 'icon-home'},
-        {path: '/about', name: '关于', style: 'icon-user'},
-        {path: '/test1', name: '关于', style: 'icon-user'},
-        {path: '/test2', name: '关于', style: 'icon-user'},
-        {path: '/test3', name: '关于', style: 'icon-user'},
-        {path: '/test4', name: '关于', style: 'icon-user'},
-        {path: '/admin', name: '管理员入口', style: 'icon-user'}
+        { path: '/', name: '主页', style: 'icon-home' },
+        { path: '/about', name: '关于', style: 'icon-user' },
+        { path: '/test1', name: '关于', style: 'icon-user' },
+        { path: '/test2', name: '关于', style: 'icon-user' },
+        { path: '/test3', name: '关于', style: 'icon-user' },
+        { path: '/test4', name: '关于', style: 'icon-user' },
+        { path: '/admin', name: '管理员入口', style: 'icon-user' }
       ]
     }
   }
