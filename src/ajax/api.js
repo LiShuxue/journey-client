fcef91de99e15e05c449dd49ec876fbcb31580e3
Li Shuxue <Shuxue.Li@sc.com>
2018-09-07 10:50:25 +0800

    前端登录验证---请求头加上Authorization header

--- src/ajax/api.js
+++ src/ajax/api.js
@@ -1,13 +1,15 @@
 // admin's
 const login = `api/admin/login`
 const register = `api/admin/register`
+const test = `api/admin/test`
 
 // visitor's
 
 export default {
   admin: {
     login,
-    register
+    register,
+    test
   },
   visitor: {}
 }
--- src/ajax/config.js
+++ src/ajax/config.js
@@ -1,9 +1,18 @@
 import axios from 'axios'
+import API from './api'
+import store from '../store'
 
 axios.defaults.timeout = 5000
 axios.defaults.baseURL = 'http://localhost:8080'
 
 axios.interceptors.request.use(config => {
+  for (let key in API.admin) {
+    if (config.url.includes(API.admin[key]) && config.url !== 'http://localhost:8080/api/admin/login') {
+      if (store.state.token) {
+        config.headers.Authorization = `Bearer ${store.state.token}`
+      }
+    }
+  }
   return config
 }, error => {
   return Promise.reject(error)
