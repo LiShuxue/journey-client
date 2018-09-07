21f1b17ea5087ab839f85f0e915abdf664bc4a9e
LiShuxue <1149926505@qq.com>
2018-09-07 10:50:25 +0800

    前端登录验证---请求头加上Authorization header

--- src/views/admin/home.vue
+++ src/views/admin/home.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="home">
       this is after login
+      <el-button type="info" @click="test">test</el-button>
   </div>
 </template>
 
 <script>
+import API from '@/ajax/api.js'
+export default {
+  methods: {
+    test () {
+      this.axios.get(API.admin.test).then(response => {
+        console.log(response.data)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
+}
 </script>
 
 <style scoped>
