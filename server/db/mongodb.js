83b93990f8c41e51ef6f1bf4c5d2be39670be7cc
Li Shuxue <Shuxue.Li@sc.com>
2018-09-13 12:04:38 +0800

    命令行一键启动

--- server/db/mongodb.js
+++ server/db/mongodb.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
 const db_config = require('./db_config');
+const db_path = 'mongodb://' + db_config.host + ':' + db_config.port + '/' + db_config.name
 
-mongoose.connect('mongodb://' + db_config.host + ':' + db_config.port + '/' + db_config.name);
-mongoose.Promise =  global.Promise
+mongoose.connect(db_path);
+mongoose.Promise =  global.Promise;
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+let maxConnectTimes = 0;
+db.on('error', err=>{
+  console.log('DB connect failed...');
+  if(maxConnectTimes < 5){
+    maxConnectTimes++;
+    console.log('DB connect again...');
+    mongoose.connect(db_path);
+  }else{
+    console.log(`DB can't connect now becuase some reason...`);
+  }
+});
 db.once('open', ()=>{
   console.log('DB connected...')
 });
