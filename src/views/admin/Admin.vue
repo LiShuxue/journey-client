f778edd0606a49fb2ff11fc21ac231439184aba2
Li Shuxue <Shuxue.Li@sc.com>
2018-09-14 12:03:06 +0800

    添加管理员菜单

--- src/views/admin/Admin.vue
+++ src/views/admin/Admin.vue
@@ -1,17 +1,56 @@
 <template>
   <div class="admin">
-    <transition name="router-fade" mode="out-in">
-      <router-view></router-view>
-    </transition>
+    <el-row>
+      <el-col :span="4">
+        <el-menu
+          default-active="1"
+          class="nav-list"
+          @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#409EFF">
+          <el-menu-item index="home">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航一</span>
+          </el-menu-item>
+          <el-menu-item index="test">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+
+      <el-col :span="20">
+        <div class="content-wrapper">
+          <transition name="router-fade" mode="out-in">
+            <router-view></router-view>
+          </transition>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+      this.$router.push(key)
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.nav-list{
+  height: 100vh;
+}
+
+.content-wrapper{
+  margin: 10px 0 0 20px;
+}
+
 .router-fade-enter{
     opacity: 0;
 }
