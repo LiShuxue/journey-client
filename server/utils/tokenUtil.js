89427aa82afdfcd91486e06e7aa0d1f500c97d8c
LiShuxue <1149926505@qq.com>
2018-09-17 11:54:41 +0800

    access_token过期之后使用refresh_token生成新的token，并返回给前台

--- server/utils/tokenUtil.js
+++ server/utils/tokenUtil.js
@@ -1,12 +1,13 @@
 const secret = 'secret-key';
 const jwt = require('jsonwebtoken');
+const UserModel = require('../models/User');
 
-const initPayload = {
+let initPayload = {
     iss: "Journey", //(Issuer) jwt签发者
     sub: "test@example.com", //(Subject) 该jwt所面向的用户
     aud: "www.example.com", //(Audience) 接收jwt的一方
-    iat: Date.now()/1000, //(Issued At) jwt的签发时间，单位秒s
-    exp: Date.now()/1000 + 5*60 //(Expiration Time) jwt的过期时间，单位秒s
+    iat: Math.floor(Date.now()/1000), //(Issued At) jwt的签发时间，单位秒s
+    exp: Math.floor(Date.now()/1000) + 5*60 //(Expiration Time) jwt的过期时间，单位秒s
 };
 
 const createAccessToken = (payload)=>{
@@ -18,20 +19,57 @@ const createAccessToken = (payload)=>{
 const createRefreshToken = ()=>{
     return createAccessToken({
         target: 'refresh',
-        exp: Date.now()/1000 + 5*60*60
+        exp: Math.floor(Date.now()/1000) + 5*60*60
     })
 }
 
-const verifyAccessToken = ( ctx, next ) => {
-    const authorization = ctx.get('Authorization');
-    if (authorization === '') {
-        ctx.throw(401, `no token detected in http header 'Authorization'`);
+const verifyRefreshToken = async (refresh_token, access_token) => {
+    try{
+        jwt.verify(refresh_token, secret);
+    }catch(err){
+        return Promise.reject(err);
     }
-    const token = authorization.split(' ')[1];
+
+    let decoded = jwt.decode(access_token);
+    let username = decoded.username;
+    let new_access_token = createAccessToken({username});
+    let new_refresh_token = createRefreshToken();
+    let user, result;
+    try{
+        user = await UserModel.getUser(username);
+        result = await Promise.all([
+            UserModel.saveAccessToken(user, new_access_token),
+            UserModel.saveRefreshToken(user, new_refresh_token)
+        ]);
+    }catch(err){
+        return Promise.reject(err);
+    }
+    return Promise.resolve({
+        new_access_token,
+        new_refresh_token
+    })
+}
+
+const verifyAccessToken = async ( ctx, next ) => {
+    const authorization = ctx.get('Authorization');
+    const access_token = authorization.split(' ')[1];
+    const refresh_token = ctx.get('refresh_token');
+
+    if (access_token === '' || refresh_token === '') {
+        ctx.throw(401, `No access_token/refresh_token detected in http headers`);
+    }
+    
     try {
-        let payload = jwt.verify(token, secret);
-    } catch (err) {
-        ctx.throw(401, err);
+        jwt.verify(access_token, secret);
+    }catch(err){
+        if(err.name === 'TokenExpiredError'){
+            let result = await verifyRefreshToken(refresh_token, access_token).catch((err)=>{
+                ctx.throw(401, err);
+            });
+            ctx.token = result;
+        }else{
+            ctx.throw(401, err);
+        }
     }
     next();
 }
--- src/ajax/config.js
+++ src/ajax/config.js
@@ -9,8 +9,9 @@ axios.interceptors.request.use(config => {
   for (let key in API.admin) {
     // 除了登录注册之外的管理员相关请求，需要携带token
     if (config.url.includes(API.admin[key]) && config.url !== 'http://localhost:8080/api/admin/login' && config.url !== 'http://localhost:8080/api/admin/register') {
-      if (store.state.access_token) {
+      if (store.state.access_token && store.state.refresh_token) {
         config.headers.Authorization = `Bearer ${store.state.access_token}`
+        config.headers.refresh_token = store.state.refresh_token
       }
     }
   }
@@ -23,6 +24,10 @@ axios.interceptors.response.use(response => {
   console.log(response)
   if (response.status === 200 && response.data.errMsg) {
     return Promise.reject(response)
+  }
+  if (response.status === 200 && response.data.new_access_token && response.data.new_refresh_token) {
+    store.dispatch('saveAccessTokenAction', response.data.new_access_token)
+    store.dispatch('saveRefreshTokenAction', response.data.new_refresh_token)
   }
   return response
 }, error => {
