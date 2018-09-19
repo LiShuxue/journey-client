beee1d4c6e9b5a09366a763be73bfcf1eb4da9ac
LiShuxue <1149926505@qq.com>
2018-09-19 16:49:53 +0800

    删掉以前用于测试的test方法，并修改API的结构

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,9 @@
 const UserController = require('../controllers/userController.js');
 const Router = require('koa-router');
-const tokenUtil = require('../utils/tokenUtil');
 
 const userRoute = new Router();
 
 userRoute.post('/login', UserController.login);
 userRoute.post('/register', UserController.register);
-userRoute.get('/test', tokenUtil.verifyAccessToken, UserController.test);
 
 module.exports = userRoute;
--- src/ajax/api.js
+++ src/ajax/api.js
@@ -1,17 +1,16 @@
-// admin's
+// require auth api call
+const publish = `api/blog/publish`
+
+// not require auth api call
 const login = `api/admin/login`
 const register = `api/admin/register`
-const publish = `api/blog/publish`
-const test = `api/admin/test`
-
-// visitor's
 
 export default {
-  admin: {
-    login,
-    register,
-    publish,
-    test
+  requireAuth: {
+    publish
   },
-  visitor: {}
+  notRequireAuth: {
+    login,
+    register
+  }
 }
