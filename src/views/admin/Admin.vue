dac18ba10ea52ab41dd4a2b4a39b10c102f4237d
Li Shuxue <Shuxue.Li@sc.com>
2018-09-19 16:57:00 +0800

    完成博客编辑页面的UI

--- src/views/admin/Admin.vue
+++ src/views/admin/Admin.vue
@@ -1,33 +1,27 @@
 <template>
   <div class="admin">
-    <el-row>
-      <el-col :span="4">
-        <el-menu
-          default-active="1"
-          class="nav-list"
-          @select="handleSelect"
-          background-color="#545c64"
-          text-color="#fff"
-          active-text-color="#409EFF">
-          <el-menu-item index="home">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航一</span>
-          </el-menu-item>
-          <el-menu-item index="edit">
-            <i class="el-icon-menu"></i>
-            <span slot="title">写博客</span>
-          </el-menu-item>
-        </el-menu>
-      </el-col>
+    <el-menu
+      default-active="1"
+      class="nav-list"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#409EFF">
+      <el-menu-item index="home">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航一</span>
+      </el-menu-item>
+      <el-menu-item index="edit">
+        <i class="el-icon-menu"></i>
+        <span slot="title">写博客</span>
+      </el-menu-item>
+    </el-menu>
 
-      <el-col :span="20">
-        <div class="content-wrapper">
-          <transition name="router-fade" mode="out-in">
-            <router-view></router-view>
-          </transition>
-        </div>
-      </el-col>
-    </el-row>
+    <div class="content-wrapper">
+      <transition name="router-fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -44,11 +38,16 @@ export default {
 
 <style lang="scss" scoped>
 .nav-list{
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 17vw;
   height: 100vh;
 }
 
 .content-wrapper{
   padding: 10px 20px 10px 20px;
+  margin-left: 17vw;
 }
 
 .router-fade-enter{
