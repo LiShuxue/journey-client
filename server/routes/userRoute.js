8389033ab16b318e1d4596485e8d38066bab2bf6
LiShuxue <1149926505@qq.com>
2018-10-15 11:33:28 +0800

    将后端的代码分离出去，创建一个新的项目

--- server/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const UserController = require('../controllers/userController.js');
-const Router = require('koa-router');
-
-const userRoute = new Router();
-
-userRoute.post('/login', UserController.login);
-userRoute.post('/register', UserController.register);
-
-module.exports = userRoute;
