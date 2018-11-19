72ebc89ea1c9fe58099fe47833b8de4d52817a80
Li Shuxue <Shuxue.Li@sc.com>
2018-11-19 15:14:33 +0800

    if run in mobile will add is-mobile flag

---
+++ src/utils/device.js
@@ -0,0 +1,10 @@
+const userAgent = window.navigator.userAgent;
+const isIOS = /(iPhone|iPad|iPod|iOS)/i.test(userAgent);
+const isAndroid = userAgent.includes('Android');
+const isMobile = isAndroid || isIOS;
+
+export {
+  isIOS,
+  isAndroid,
+  isMobile
+}
