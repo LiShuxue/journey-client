d35231b7f98ecf4f0a49e1158f7e40da8d15fa6c
LiShuxue <1149926505@qq.com>
2019-07-15 17:32:27 +0800

    add user list page

--- src/ajax/api.js
+++ src/ajax/api.js
@@ -3,21 +3,25 @@ const publish = `blog/publish`
 const addCategory = `blog/category/add`
 const uploadToken = `qiniu/uploadToken`
 const removeImage = `qiniu/removeImage`
+const userList = `admin/list`
+const deleteUser = `admin/delete`
+const updateUser = `admin/update`
 
 // not require auth api call
 const login = `admin/login`
 const register = `admin/register`
 const categoryList = `blog/category/list`
 const blogList = `blog/list`
-// const hotBlogList = `blog/hot`
-// const tagList = `blog/tag/list`
 
 export default {
   requireAuth: {
     publish,
     addCategory,
     uploadToken,
-    removeImage
+    removeImage,
+    userList,
+    deleteUser,
+    updateUser
   },
   notRequireAuth: {
     login,
