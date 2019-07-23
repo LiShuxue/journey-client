026240ba667d905b234fad900f40e5c64027a9df
LiShuxue <1149926505@qq.com>
2019-07-23 17:46:24 +0800

    upload sourcemap to sentry

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const gitSha = require('child_process').execSync('git rev-parse HEAD').toString().trim() // 这个是获取提交版本的记录
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+
+process.env.SENTRY_RELEASE_VERSION = gitSha
+
 module.exports = {
   baseUrl: './',
   css: {
@@ -6,6 +11,17 @@ module.exports = {
       sass: {
         data: `@import "@/assets/style/base.scss";`
       }
+    }
+  },
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // sentry upload sourcemap
+      config.plugin('sentry').use(SentryWebpackPlugin, [{
+        ignore: ['node_modules'],
+        include: './dist', // 上传dist文件的js
+        configFile: './.sentryclirc', // 配置文件地址
+        release: process.env.SENTRY_RELEASE_VERSION // 版本号
+      }])
     }
   }
 }
