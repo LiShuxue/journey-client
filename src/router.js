5ded92b6e06768cbb3fe72b49e8f4ddf3eaa671e
LiShuxue <1149926505@qq.com>
2019-09-02 17:01:32 +0800

    add route name and not show the right box in about route

--- src/router.js
+++ src/router.js
@@ -25,9 +25,9 @@ const router = new Router({
       component: () => import('./views/visitor/Main.vue'),
       redirect: '/home',
       children: [
-        { path: 'home', component: () => import('./views/visitor/Home.vue'), meta: { keepAlive: true } },
-        { path: 'category', component: () => import('./views/visitor/Category.vue') },
-        { path: 'about', component: () => import('./views/visitor/About.vue') },
+        { path: 'home', name: 'home', component: () => import('./views/visitor/Home.vue'), meta: { keepAlive: true } },
+        { path: 'category', name: 'category', component: () => import('./views/visitor/Category.vue') },
+        { path: 'about', name: 'about', component: () => import('./views/visitor/About.vue') },
         { path: 'blog', name: 'blog', component: () => import('./views/visitor/Blog.vue') },
         { path: 'bloglist', name: 'bloglist', component: () => import('./views/visitor/BlogList.vue') }
       ]
