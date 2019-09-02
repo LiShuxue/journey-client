8153b7c5e83325a2760c09cb363d47f01e357ff5
LiShuxue <1149926505@qq.com>
2019-09-02 14:23:31 +0800

    complete category page

--- src/views/visitor/Category.vue
+++ src/views/visitor/Category.vue
@@ -1,34 +1,65 @@
 <template>
   <div class="category">
-    <h1>架构师的成长之路</h1>
     <h3>知识，要靠一点一滴的积累</h3>
-    <hr />
     <div>
-      <p><img src="../../assets/img/vue-logo.png" alt=""><span>Vue</span><span>(20)</span></p>
-      <p><img src="../../assets/img/vue-logo.png" alt=""><span>Vue</span><span>(20)</span></p>
-      <p><img src="../../assets/img/vue-logo.png" alt=""><span>Vue</span><span>(20)</span></p>
-      <p><img src="../../assets/img/vue-logo.png" alt=""><span>Vue</span><span>(20)</span></p>
-      <p><img src="../../assets/img/vue-logo.png" alt=""><span>Vue</span><span>(20)</span></p>
-      <p><img src="../../assets/img/vue-logo.png" alt=""><span>Vue</span><span>(20)</span></p>
-      <p><img src="../../assets/img/vue-logo.png" alt=""><span>Vue</span><span>(20)</span></p>
+      <p v-for="(category, index) in categoryList" :key="index" @click="clickCategory(category)">
+        <span class="icon iconfont icon-category"></span>
+        <span>{{category}}</span>
+        <span>（{{blogList.filter(value=>{return value.category===category}).length}}）</span>
+      </p>
     </div>
   </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState([
+      'blogList'
+    ]),
+    ...mapGetters([
+      'categoryList'
+    ])
+  },
+  methods: {
+    clickCategory(category) {
+      let blogListWithSameCategory = this.blogList.filter((value) => {
+        return value.category === category
+      })
+      this.$store.commit('saveFilterBlogList', blogListWithSameCategory)
+      this.$router.push('/bloglist')
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 .category{
   box-sizing: border-box;
   border-radius: 10px;
-  padding: 20px 20px 40px 20px;
+  padding: 20px 20px 20px 20px;
+  margin-bottom: 40px;
   background: $hui-bai;
   color: $hui-hei;
 
   p{
     display: flex;
     align-items: center;
-    img{
+    width: 150px;
+    background: $qian-hui;
+    padding: 5px;
+    margin: 0 10px 10px 0;
+    border-radius: 10px;
+    cursor: pointer;
+    font-size: $mediu-size;
+    &:hover{
+      background: $shen-hui;
+    }
+    .icon{
       width: 20px;
-      margin-right: 10px;
+      margin-right: 5px;
     }
     :nth-child(2){
       margin-right: 5px;
