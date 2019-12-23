1936384cfcef651ae3f4b74d96cf807b108ad272
LiShuxue <1149926505@qq.com>
2019-12-23 10:41:14 +0800

    remove unused code

--- src/ajax/api.js
+++ src/ajax/api.js
@@ -2,7 +2,6 @@
 const publishBlog = `blog/publish`
 const updateBlog = `blog/update`
 const deleteBlog = `blog/delete`
-const addCategory = `blog/category/add`
 const uploadToken = `qiniu/uploadToken`
 const removeImage = `qiniu/removeImage`
 const userList = `admin/list`
@@ -12,7 +11,6 @@ const updateUser = `admin/update`
 // not require auth api call
 const login = `admin/login`
 const register = `admin/register`
-const categoryList = `blog/category/list`
 const blogList = `blog/list`
 const blogDetail = `blog/detail`
 const likeBlog = `blog/like`
@@ -22,7 +20,6 @@ export default {
     publishBlog,
     updateBlog,
     deleteBlog,
-    addCategory,
     uploadToken,
     removeImage,
     userList,
@@ -32,7 +29,6 @@ export default {
   notRequireAuth: {
     login,
     register,
-    categoryList,
     blogList,
     blogDetail,
     likeBlog
